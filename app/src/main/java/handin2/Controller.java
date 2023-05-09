dcb773195ad23f7f480a43efc829e7922844b52f
palleBrandt <lupa@itu.dk>
2023-05-09 09:58:18 +0200

    Redraw comments

--- app/src/main/java/handin2/Controller.java
+++ app/src/main/java/handin2/Controller.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
--- app/src/main/java/handin2/Node.java
+++ app/src/main/java/handin2/Node.java
@@ -31,6 +31,7 @@ public class Node implements Serializable {
             return false;
     }
 
+    //Used the "afstandformel" to determine the distance between two points in a 2 dimensional space
     public double distToVertex(Node other) {
         double dist = (Math.pow(Math.abs((lat-(other.lat))),2.0) + Math.pow(Math.abs(((lon-other.lon)*0.56)),2.0));
         return Math.sqrt(dist);
