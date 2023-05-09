ca2e6793afa123f22f988152c74913632d995e24
rhra <rhra@itu.dk>
2023-05-09 12:44:08 +0200

    view cleanup

--- app/src/main/java/handin2/View.java
+++ app/src/main/java/handin2/View.java
@@ -27,7 +27,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.FillRule;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
@@ -40,33 +39,25 @@ import javafx.scene.image.ImageView;
 
 
 public class View {
-    Layout layout = new Layout();
-    TextField fromSearch = new TextField("");
-    TextField toSearch = new TextField("");
-    Address TSTadr = null;
-    
-    Colorscheme colors;
-    Canvas canvas = new Canvas(640, 480);
     double canvasHeighScale = 1;
     double canvasWidthScale = 1;
 
+    Canvas canvas = new Canvas(640, 480);
     GraphicsContext gc = canvas.getGraphicsContext2D();
-
     Affine trans = new Affine();
-    Label scaleLabel;
     Scalebar scalebar;
+    Colorscheme colors;
     Model model;
     Position position;
+    Layout layout = new Layout();
+    Address TSTadr;
     Scene scene;
-    Label nearestRoad;
-
-    // Debugger
+    
+    MenuItem fileChoserMenu;
+    CheckMenuItem showDebugger;
     CheckMenuItem freezeFrame;
-    CheckMenuItem showOnlyRoads;
     Label mousePositionLabel = new Label();
     Label redrawTimerLabel = new Label();
-    BorderPane debuggerOverlay;
-    CheckMenuItem showDebugger;
 
     ArrayList<Address> POI = new ArrayList<Address>();
     Address[] routeAdrs = new Address[2];
@@ -78,121 +69,93 @@ public class View {
     Way path;
     String transportType = "car";
 
-    // search elements
     ImageView serachLoopImage;
     ImageView findRouteImage;
     ImageView turnLeftImage;
     ImageView turnRightImage;
-
-    StackPane searchStackpan;
-
-    Boolean search = false;
-    VBox searchResultVBox;
-    TextField searchTextField;
+    
     Button favoritesButton;
     Button searchMenu;
-    Button findRouteMenu;
-    Boolean foundAddress = false;
+    Button findRouteMenu;   
+    Button favoritesButton2;
+    Button routeDescriptionButton;
+    Button swapButton;
     Button favoriteMenu;
     Button settingsMenu;
-    StackPane settingStackPane;
     Button clearFavoritesButton;
     Button clearFindRouteButton;
     Button walkButton;
     Button bikeButton;
     Button carButton;
-
-    StackPane findRouteStackPane;
-    TextField findRouteFromTextField;
-    TextField findRouteToTextField;
-    VBox searchResultFromVBox;
-    VBox searchResultToVBox;
-    Button favoritesButton2;
-    Button routeDescriptionButton;
-    Button swapButton;
-
-    StackPane favoriteStackPane;
-    VBox favoritListVBox;
-    HBox routeInfoHBox;
-
-    StackPane routeDescriptionStackPane;
     Button routeDescriptionCloseButton;
     Button copyButton;
-    HBox routeDescriptionHhox;
-    VBox routeDescriptionInstruction;
-    ScrollPane routeDescriptionScrollpane;
-
-    MenuItem fileChoserMenu;
+    
+    Label scaleLabel;
+    Label nearestRoad;
     Rectangle favoritBackgound;
 
+    TextField searchTextField;
+    TextField findRouteFromTextField;
+    TextField findRouteToTextField;
+
+    VBox routeDescriptionInstruction;
+    VBox searchResultFromVBox;
+    VBox searchResultToVBox;
+    VBox searchResultVBox;
+    
+    VBox favoritListVBox;
+    HBox routeInfoHBox;
+    HBox routeDescriptionHhox;
+
+    ScrollPane routeDescriptionScrollpane;
+
+    StackPane searchStackpan;
+    StackPane settingStackPane;
+    StackPane favoriteStackPane;
+    StackPane routeDescriptionStackPane;
+    StackPane findRouteStackPane;
+    StackPane debuggerOverlay;
+
     public View(Model model, Stage primaryStage) {
-        gc.setFillRule(FillRule.EVEN_ODD);
         this.model = model;
         colors = new Colorscheme(); // Creates coclorscheme
-
-        primaryStage.setTitle("GoKort");
-        primaryStage.getIcons().add(new Image("file:icons/logo.jpg"));
-
-        BorderPane mapBoarderPane = new BorderPane();
-
-        MenuBar menuBar = new MenuBar();
-
-        Menu toolMenu = new Menu("Tools");
-
-        freezeFrame = new CheckMenuItem("Freeze Canvas");
-        showDebugger = new CheckMenuItem("Show DebuggerOverlay");
-        showOnlyRoads = new CheckMenuItem("Show Only Roads");
-        toolMenu.getItems().add(freezeFrame);
-        toolMenu.getItems().add(showDebugger);
-        toolMenu.getItems().add(showOnlyRoads);
-
+        
+     
         VBox debuggerVbox = new VBox();
-        debuggerVbox.setLayoutX(200);
-        debuggerVbox.setLayoutY(100);
         debuggerVbox.setAlignment(Pos.TOP_RIGHT);
-        debuggerVbox.getChildren().add(mousePositionLabel);
-        debuggerVbox.getChildren().add(redrawTimerLabel);
-        debuggerOverlay = new BorderPane();
-        debuggerOverlay.setMouseTransparent(true);
-        debuggerOverlay.setVisible(false);
-        debuggerOverlay.setCenter(debuggerVbox);
+        debuggerVbox.getChildren().addAll(mousePositionLabel,redrawTimerLabel);
+        debuggerOverlay = layout.getStackPane(100,100);
+        debuggerOverlay.getChildren().addAll(debuggerVbox);
 
-        // New layout ------------------------------------------------------------
-        searchResultVBox = layout.getSearchResultVbox();
-        searchTextField = layout.getTextField("Skriv adresse");
-
+        // UI design 
+        //images that is being copied for favoritelist and directions
         turnLeftImage = layout.getImageView("file:icons/turnLeft.png");
         turnRightImage = layout.getImageView("file:icons/turnRight.png");
         serachLoopImage = layout.getImageView("file:icons/searchLoop.png");
         findRouteImage = layout.getImageView("file:icons/findRoute.png");
-        searchMenu = layout.getButtonIcon(serachLoopImage, 20);
-        searchMenu.setTooltip(new Tooltip("Find address"));
+        
+        //elements for searching stackpane
+        searchTextField = layout.getTextField("Skriv adresse");
+        searchResultVBox = layout.getSearchResultVbox();
         favoritesButton = layout.getButtonIcon(layout.getImageView("file:icons/starBlack.png"), 20);
         favoritesButton.setTooltip(new Tooltip("Add address to favoritelist"));
 
-        findRouteMenu = layout.getButtonIcon(findRouteImage, 20);
-        findRouteMenu.setTooltip(new Tooltip("Find route"));
-        favoriteMenu = layout.getButtonIcon(layout.getImageView("file:icons/bookmark.png"), 20);
-        favoriteMenu.setTooltip(new Tooltip("Favoritelist"));
-        settingsMenu = layout.getButtonIcon(layout.getImageView("file:icons/settings.png"), 20);
-        settingsMenu.setTooltip(new Tooltip("Settings"));
-        clearFavoritesButton = layout.getButtonIcon(layout.getImageView("file:icons/clear.png"), 20);
-        clearFavoritesButton.setTooltip(new Tooltip("Delete favoritelist"));
 
+        //adds all elements to search stackpane and makes the insets
         searchStackpan = layout.getStackPane(40, 340);
         searchStackpan.getChildren().addAll(searchTextField, searchResultVBox, favoritesButton);
         StackPane.setMargin(searchTextField, new Insets(0, 0, 0, 20));
         StackPane.setMargin(searchResultVBox, new Insets(40, 0, 0, 40));
         StackPane.setMargin(favoritesButton, new Insets(0, 0, 0, 300));
 
+        
+        //elements for find route stackpane
         carButton = layout.getButtonIcon(layout.getImageView("file:icons/car.png"), 20);
         carButton.setBackground(new Background(new BackgroundFill(Color.MAGENTA, null, null)));
         bikeButton = layout.getButtonIcon(layout.getImageView("file:icons/bike.png"), 20);
         walkButton = layout.getButtonIcon(layout.getImageView("file:icons/walk.png"), 20);
         clearFindRouteButton = layout.getButtonIcon(layout.getImageView("file:icons/clear.png"), 20);
         clearFindRouteButton.setTooltip(new Tooltip("Delete search"));
-
-
         findRouteFromTextField = layout.getTextField("Find vej fra");
         findRouteToTextField = layout.getTextField("Find vej til");
         searchResultFromVBox = layout.getSearchResultVbox();
@@ -203,36 +166,38 @@ public class View {
         routeDescriptionButton.setTooltip(new Tooltip("Show directions"));
         swapButton = layout.getButtonIcon(layout.getImageView("file:icons/swap.png"), 20);
         swapButton.setTooltip(new Tooltip("Swap destinations"));
-
         HBox veichleOption = new HBox(carButton, bikeButton, walkButton);
         veichleOption.setSpacing(10);
         veichleOption.setAlignment(Pos.BASELINE_CENTER);
-
         Shape box = layout.getRegtangle(130, 340);
+
+        //adds all elements to route stackpane and Insets elements
         findRouteStackPane = layout.getStackPane(100, 300);
         findRouteStackPane.getChildren().addAll(box, findRouteFromTextField, findRouteToTextField, veichleOption,
-                searchResultFromVBox, searchResultToVBox, favoritesButton2, swapButton, routeDescriptionButton,
-                clearFindRouteButton);
-
+            searchResultFromVBox, searchResultToVBox, favoritesButton2, swapButton, routeDescriptionButton, clearFindRouteButton);
         StackPane.setMargin(clearFindRouteButton, new Insets(0, 0, 90, 300));
         StackPane.setMargin(favoritesButton2, new Insets(45, 0, 45, 300));
         StackPane.setMargin(routeDescriptionButton, new Insets(90, 0, 0, 300));
         StackPane.setMargin(findRouteFromTextField, new Insets(0, 0, 0, 20));
         StackPane.setMargin(searchResultFromVBox, new Insets(40, 0, 0, 40));
         StackPane.setMargin(swapButton, new Insets(45, 0, 0, 0));
-
         StackPane.setMargin(veichleOption, new Insets(90, 0, 0, 0));
         StackPane.setMargin(findRouteToTextField, new Insets(45, 0, 0, 20));
         StackPane.setMargin(searchResultToVBox, new Insets(85, 0, 0, 40));
 
-        favoriteStackPane = layout.getStackPane(40, 340);
+         //elements for favoritelist
         favoritBackgound = layout.getRegtangle(40, 340);
         favoritListVBox = new VBox();
+        clearFavoritesButton = layout.getButtonIcon(layout.getImageView("file:icons/clear.png"), 20);
+        clearFavoritesButton.setTooltip(new Tooltip("Delete favoritelist"));
 
+        //adds all elements to favoritelist stackpane and Insets elements
+        favoriteStackPane = layout.getStackPane(40, 340);
         favoriteStackPane.getChildren().addAll(favoritBackgound, favoritListVBox, clearFavoritesButton);
         StackPane.setMargin(favoritListVBox, new Insets(40, 0, 0, 0));
         StackPane.setMargin(clearFavoritesButton, new Insets(0, 0, 0, 300));
 
+        //elemnts for route instruction
         Shape backgroundBox = layout.getRegtangle(280, 340);
         routeDescriptionHhox = new HBox();
         routeDescriptionCloseButton = layout.getButtonIcon(layout.getImageView("file:icons/close.png"), 20);
@@ -240,29 +205,28 @@ public class View {
         copyButton = layout.getButtonIcon(layout.getImageView("file:icons/copy.png"), 20);
         copyButton.setTooltip(new Tooltip("Copy to clipboard"));
         routeDescriptionInstruction = layout.getSearchResultVbox();
-       
-        
         routeDescriptionScrollpane = new ScrollPane(routeDescriptionInstruction);
         routeInfoHBox = new HBox();
+
+         //adds all elements to route instructions stackpane and Insets elements
         routeDescriptionStackPane = layout.getStackPane(280, 340);
         routeDescriptionStackPane.getChildren().addAll(backgroundBox, routeInfoHBox, routeDescriptionScrollpane,routeDescriptionCloseButton, copyButton);
         StackPane.setMargin(routeDescriptionCloseButton, new Insets(0, 0, 0, 300));
         StackPane.setMargin(routeDescriptionScrollpane, new Insets(40, 0, 20, 0));
         StackPane.setMargin(routeInfoHBox, new Insets(10, 0, 0, 40));
 
+        //elements for setting menu
         Menu fileMenu = new Menu("File");
         File file = new File("data");
 
+        //user can upload their own files, endning with osm or zip.
         String[] pathnames = file.list();
-
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open File");
         fileChooser.setInitialDirectory(new File(System.getProperty("user.home") + "/Downloads"));
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("OSM files", "*.osm", "*.osm.zip"));
-
         fileChoserMenu = new MenuItem("Select file");
         fileChoserMenu.setOnAction(e -> {
-
             File file2 = fileChooser.showOpenDialog(primaryStage);
             if (file2 != null) {
                 try {
@@ -272,19 +236,15 @@ public class View {
                 } catch (ClassNotFoundException | IOException | XMLStreamException | FactoryConfigurationError e1) {
                     e1.printStackTrace();
                 }
-
             }
-
         });
 
+        //gets themes from colorSchemes and add them to menuItem
         for (String name : pathnames) {
             MenuItem menuItem = new MenuItem(name);
             menuItem.setOnAction(e -> {
-                try {
-                    System.out.println(name);
+                try {             
                     var model2 = Model.load("data/" + name);
-                    System.out.println("data/" + name);
-
                     View view = new View(model2, primaryStage);
                     new Controller(model2, view);
                 } catch (ClassNotFoundException | IOException | XMLStreamException | FactoryConfigurationError e1) {
@@ -294,9 +254,9 @@ public class View {
             fileMenu.getItems().add(menuItem);
         }
 
+        //gets themes from colorSchemes and add them to menuItem
         Menu colorMenu = new Menu("Color");
         ArrayList<String> themes = colors.getThemes();
-
         for (String name : themes) {
             MenuItem menuItem = new MenuItem(name);
             menuItem.setOnAction(e -> {
@@ -305,62 +265,75 @@ public class View {
             });
             colorMenu.getItems().add(menuItem);
         }
+        MenuBar menuBar = new MenuBar();
+        Menu toolMenu = new Menu("Tools");
+        freezeFrame = new CheckMenuItem("Freeze Canvas");
+        showDebugger = new CheckMenuItem("Show DebuggerOverlay");
+        toolMenu.getItems().addAll(freezeFrame,showDebugger);
 
-        SeparatorMenuItem separator = new SeparatorMenuItem();
-        fileMenu.getItems().add(separator);
-        fileMenu.getItems().add(fileChoserMenu);
+        fileMenu.getItems().addAll(new SeparatorMenuItem(),fileChoserMenu);
         menuBar.getMenus().addAll(fileMenu, colorMenu, toolMenu);
 
+        //adds elements to settings stackpane and insets elements        
         settingStackPane = layout.getStackPane(40, 200);
         settingStackPane.getChildren().add(menuBar);
         StackPane.setMargin(menuBar, new Insets(10, 0, 0, 30));
 
-        StackPane stackPane = new StackPane(mapBoarderPane, searchStackpan, settingStackPane, settingsMenu,
-                favoriteStackPane, favoriteMenu, findRouteStackPane, routeDescriptionStackPane, findRouteMenu,
-                searchMenu, debuggerOverlay);
-        stackPane.setAlignment(Pos.TOP_LEFT);
-        int buttonDistance = 45;
-        // top, right, bottom, left
-        StackPane.setMargin(searchMenu, new Insets(10, 0, 0, 10));
-        StackPane.setMargin(searchStackpan, new Insets(10, 0, 0, 10));
-
-        StackPane.setMargin(findRouteMenu, new Insets(buttonDistance + 10, 0, 0, 10));
-        StackPane.setMargin(findRouteStackPane, new Insets(buttonDistance + 10, 0, 0, 10));
-        StackPane.setMargin(routeDescriptionStackPane, new Insets(190, 0, 0, 10));
-
-        StackPane.setMargin(favoriteStackPane, new Insets(buttonDistance * 2 + 10, 0, 0, 10));
-        StackPane.setMargin(favoriteMenu, new Insets(buttonDistance * 2 + 10, 0, 0, 10));
-
-        StackPane.setMargin(settingsMenu, new Insets(buttonDistance * 3 + 10, 0, 0, 10));
-        StackPane.setMargin(settingStackPane, new Insets(buttonDistance * 3 + 10, 0, 0, 10));
-
-        StackPane.setAlignment(debuggerOverlay, Pos.BOTTOM_LEFT);
-
+        //mapboarderpane
+        //scalebar drawn in the bottom
         int scalebarLineStart = 5;
         int scalebarLineWidth = (int) (canvas.getHeight() * 0.1) + scalebarLineStart;
         int scalebarLineHight = 10;
-
         Line line1 = new Line(scalebarLineStart, scalebarLineHight / 2, scalebarLineWidth, scalebarLineHight / 2);
         Line line2 = new Line(scalebarLineStart, 0, scalebarLineStart, scalebarLineHight);
         Line line3 = new Line(scalebarLineWidth, 0, scalebarLineWidth, scalebarLineHight);
         Group scalebarLine = new Group(line1, line2, line3);
-
         scalebar = new Scalebar(model.maxlat - model.minlat, (float) (canvas.getHeight()),
                 (float) (line1.getEndX() - line1.getStartX()));
         scaleLabel = new Label(scalebar.getScaleLabel());
-        position = new Position(scalebar.getScale(), canvas, model);
-
-        mapBoarderPane.setCenter(canvas);
-
-        // Label with nearest road
         nearestRoad = new Label("Navnet på nærmeste vej");
 
+        //elements in mapboarderpane
+        BorderPane mapBoarderPane = new BorderPane();
+        mapBoarderPane.setCenter(canvas);
         HBox scalebarHBox = new HBox(scalebarLine, scaleLabel, nearestRoad);
         scalebarHBox.setSpacing(10);
         scalebarHBox.setPadding(new Insets(0, 0, 0, 10));
         mapBoarderPane.setBottom(scalebarHBox);
+        
+        
+        //Elements for root stackpane 
+        searchMenu = layout.getButtonIcon(serachLoopImage, 20);
+        searchMenu.setTooltip(new Tooltip("Find address"));
+        findRouteMenu = layout.getButtonIcon(findRouteImage, 20);
+        findRouteMenu.setTooltip(new Tooltip("Find route"));
+        favoriteMenu = layout.getButtonIcon(layout.getImageView("file:icons/bookmark.png"), 20);
+        favoriteMenu.setTooltip(new Tooltip("Favoritelist"));
+        settingsMenu = layout.getButtonIcon(layout.getImageView("file:icons/settings.png"), 20);
+        settingsMenu.setTooltip(new Tooltip("Settings"));
 
-        scene = new Scene(stackPane);
+        //rootstackpane adding element and insets
+        StackPane root = new StackPane(mapBoarderPane, searchStackpan, settingStackPane, settingsMenu,
+            favoriteStackPane, favoriteMenu, findRouteStackPane, routeDescriptionStackPane, findRouteMenu, searchMenu, debuggerOverlay);
+        root.setAlignment(Pos.TOP_LEFT);
+    
+        int buttonDistance = 45;
+        StackPane.setMargin(searchMenu, new Insets(10, 0, 0, 10));
+        StackPane.setMargin(searchStackpan, new Insets(10, 0, 0, 10));
+        StackPane.setMargin(findRouteMenu, new Insets(buttonDistance + 10, 0, 0, 10));
+        StackPane.setMargin(findRouteStackPane, new Insets(buttonDistance + 10, 0, 0, 10));
+        StackPane.setMargin(routeDescriptionStackPane, new Insets(190, 0, 0, 10));
+        StackPane.setMargin(favoriteStackPane, new Insets(buttonDistance * 2 + 10, 0, 0, 10));
+        StackPane.setMargin(favoriteMenu, new Insets(buttonDistance * 2 + 10, 0, 0, 10));
+        StackPane.setMargin(settingsMenu, new Insets(buttonDistance * 3 + 10, 0, 0, 10));
+        StackPane.setMargin(settingStackPane, new Insets(buttonDistance * 3 + 10, 0, 0, 10));
+        StackPane.setMargin(debuggerOverlay, new Insets(10, 0, 0, 100));
+       
+        //initialise last stuff for view
+        position = new Position(scalebar.getScale(), canvas, model);
+        primaryStage.setTitle("GoKort");
+        primaryStage.getIcons().add(new Image("file:icons/logo.jpg"));
+        scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
         redraw();
