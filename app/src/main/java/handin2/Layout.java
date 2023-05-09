c4b856ad84d5213261fae02ed22e559070ea4feb
aladin <asia@itu.dk>
2023-05-09 09:49:34 +0200

    time est.

--- app/src/main/java/handin2/Layout.java
+++ app/src/main/java/handin2/Layout.java
@@ -4,7 +4,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
